8471e2652dc020c1209463e4cc02b8df9d80ea0b
Mathieu Chambard <mathieu.chambard@univ-lyon1.fr>
2026-02-20 22:44:17 +0100

    test add link

--- src/apps/stable/features/search/components/SearchResults.tsx
+++ src/apps/stable/features/search/components/SearchResults.tsx
@@ -65,7 +65,7 @@ const SearchResults: FC<SearchResultsProps> = ({
                 </div>
             </div>
         );
-    }
+    } 
 
     const renderSection = (section: Section, index: number) => {
         return (
